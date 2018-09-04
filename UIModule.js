e786e4fa4647f18119a3b8d99564b5a4ac4e543e
BIT student <classroom@bgit.rs>
2018-09-04 23:44:16 +0200

    ajax

--- UIModule.js
+++ UIModule.js
@@ -1,9 +1,32 @@
-function createEventCards(arr){
-    arr.forEach(function(el){
+function createEventCards(arr) {
+    var container = document.querySelector('.container');
+    arr.forEach(function (el) {
         var div = document.createElement('div');
+        div.dataset.store = `${el}`;
+        var a = document.createElement('a');
+        a.setAttribute('href', './info.html');
         var p = document.createElement('p');
         var pText = document.createTextNode(el.name);
+        p.appendChild(pText);
+        a.appendChild(p);
         var img = document.createElement('img');
+        img.setAttribute('src', el.picture);
+        a.appendChild(img);
+        div.appendChild(a);
+        container.appendChild(div);
     })
-
 }
+
+function clickOnDiv() {
+    var divCards = document.querySelectorAll('.container div');
+    for (var i = 0; i < divCards.length; i++) {
+        (function (i) {
+            divCards[i].addEventListener('click', function () {
+                localStorage.setItem('eventObj', divCards[i].dataset.store);
+            })
+        })(i)
+    }
+}
+
+export { createEventCards,
+        clickOnDiv };
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-import arrOfEvents from './dataModule';
+import arrOfEvents from './dataModule.js';
+import { createEventCards, clickOnDiv } from './UIModule.js';
+
+var httpRequest = new XMLHttpRequest();
+httpRequest.open('GET', 'https://api.meetup.com/2/concierge?country=rs&offset=0&city=Belgrade&format=json&photo-host=public&page=500&sig_id=262525333&sig=9afb2345691a7494413f14bb4e6f70921c06206e');
+httpRequest.send();
+httpRequest.addEventListener('load', function () {
+
+        var parseObj = httpRequest.responseText;
+        createEventCards(arrOfEvents(parseObj.results))
+        clickOnDiv();
+    
+});
+
+// var apic = 'https://api.meetup.com/2/concierge?country=rs&offset=0&city=Belgrade&photo-host=public&page=500&sig_id=262525333&sig=9afb2345691a7494413f14bb4e6f70921c06206e'
+
+// fetch(apic).then(function(data){
+//     return data.json()
+// }).then(function(data){
+//             createEventCards(arrOfEvents(data.results))
+//             clickOnDiv();
+// })
