de3e8edf358a5dfa9cd4b5ac194504157054d43d
BIT student <classroom@bgit.rs>
2018-09-05 20:58:46 +0200

    page info in progress

--- UIModule.js
+++ UIModule.js
@@ -17,10 +17,10 @@ function createEventCards(arr) {
         a.appendChild(p);
         var img = document.createElement('img');
         img.setAttribute('src', el.picture);
-        if (el.picture == undefined){
+        if (el.picture == undefined) {
             img.setAttribute('src', './meetup.jpg');
         }
-        
+
         a.appendChild(img);
         div.appendChild(a);
         container.appendChild(div);
@@ -34,7 +34,7 @@ function clickOnDiv() {
             divCards[i].addEventListener('click', function () {
                 localStorage.setItem('name', divCards[i].dataset.name);
                 localStorage.setItem('picture', divCards[i].dataset.picture);
-                localStorage.setItem('description', divCards[i].dataset.description);
+                localStorage.setItem('description', JSON.stringify(divCards[i].dataset.description));
                 localStorage.setItem('city', divCards[i].dataset.city);
                 localStorage.setItem('street', divCards[i].dataset.street);
                 localStorage.setItem('longitude', divCards[i].dataset.longitude);
@@ -44,5 +44,49 @@ function clickOnDiv() {
     }
 }
 
-export { createEventCards,
-        clickOnDiv };
+function createInfoEvent(name, picture, description, city, street, longitude, latitude) {
+    var body = document.querySelector('body');
+    var header = document.createElement('header');
+    var headerText = document.createTextNode(name);
+    header.appendChild(headerText);
+    body.appendChild(header);
+    var image = document.createElement('img');
+    if (picture == 'undefined') {
+        image.setAttribute('src', './meetup.jpg');
+    } else {
+        image.setAttribute('src', picture);
+    }
+    body.appendChild(image);
+    var city1 = document.createElement('p');
+    var cityText = document.createTextNode(city)
+    city1.appendChild(cityText)
+    body.appendChild(city1);
+    var street1 = document.createElement('p');
+    var streetText = document.createTextNode(street)
+    street1.appendChild(streetText)
+    body.appendChild(street1);
+    var description1 = document.createElement('p');
+    description1.innerHTML = description;
+    body.appendChild(description1);
+
+    var divGoogle = document.createElement('div');
+    divGoogle.setAttribute('class', 'google');
+    body.appendChild(divGoogle);
+
+    var map;
+      function initMap() {
+        map = new google.maps.Map(document.querySelector('.google'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    var scriptGoogle = document.createElement('script')
+    scriptGoogle.setAttribute('src','https://maps.googleapis.com/maps/api/js?key=https://cors-anywhere.herokuapp.com/api.meetup.com/2/concierge?country=rs&offset=0&city=Belgrade&photo-host=public&page=500&sig_id=262525333&sig=9afb2345691a7494413f14bb4e6f70921c06206e&callback=initMap')
+    body.appendChild(scriptGoogle)
+}
+
+export {
+    createEventCards,
+    clickOnDiv,
+    createInfoEvent
+};
